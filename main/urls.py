672199e79970e9a366881bc70b680e92f34b2ac5
Gulzina <appazovagulzina104@gmail.com>
2025-05-12 20:23:18 +0600

    Добавлена вторая часть проекта

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about',views.about,name='about')
+    path('about',views.about,name='about'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница!',
+         'values': ['Some','Hello','123'],
+          'obj':{
+            'car':'BMW',
+            'age': 18,
+            'hobby': 'football'
+        }
+
+    }
+    return render(request, 'main/index.html',data)
 
 def about(request):
     return render(request, 'main/about.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
+
